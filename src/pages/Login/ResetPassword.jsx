058906be0f701a015c41a601bab52c107b9ffa1c
Rizky Gusti <rizkygusti808@gmail.com>
2025-02-12 13:32:12 +0700

    console.log

--- src/pages/Login/ResetPassword.jsx
+++ src/pages/Login/ResetPassword.jsx
@@ -20,6 +20,7 @@ export default function ResetPassword() {
     }
 
     try {
+      console.log('Submitting new password:', newPassword);
       await api.put("/api/users/resetpassword", {
         token: token,
         newPassword: newPassword,
