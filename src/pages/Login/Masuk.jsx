940e473975b0c65563faded70e9aa3fb1e8cbe9d
Rizky Gusti <rizkygusti808@gmail.com>
2025-02-12 19:10:09 +0700

    masuk.jsx

--- src/pages/Login/Masuk.jsx
+++ src/pages/Login/Masuk.jsx
@@ -28,10 +28,10 @@ export default function Masuk() {
         email: data.email,
         kata_sandi: data.kata_sandi, // Menggunakan kata_sandi
       });
-
+  
       if (response.data.token) {
         localStorage.setItem("token", response.data.token);
-
+  
         if (response.data.user.role === "admin") {
           navigate("/admin");
         } else {
@@ -52,17 +52,13 @@ export default function Masuk() {
 
   const handleGoogleSuccess = async (credentialResponse) => {
     try {
-      // Mengirim token dari Google ke backend untuk verifikasi
       const response = await api.post("/auth/google-login", {
-        token: credentialResponse.credential, // Token dari Google
+        token: credentialResponse.credential,
       });
 
-      // Jika login berhasil dan token diterima
       if (response.data.token) {
-        // Menyimpan token di localStorage
         localStorage.setItem("token", response.data.token);
 
-        // Menentukan halaman tujuan berdasarkan role
         if (response.data.user.role === "admin") {
           navigate("/admin");
         } else {
@@ -187,8 +183,7 @@ export default function Masuk() {
                   <GoogleLogin
                     onSuccess={handleGoogleSuccess}
                     onError={() => {
-                      // Menangani kesalahan jika login gagal
-                      toast.error("Login gagal dengan Google");
+                      // console.log("Login Failed");
                     }}
                     useOneTap
                   />
